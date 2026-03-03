ce3800fd2903cf0ab4b23583fae3cbd0f3dc1052
Felipe Alves <felipe.arnaud@aluno.uece.br>
2026-03-02 22:04:07 -0300

    core(category): Brand entity layer

---
+++ src/main/java/com/ndlcommerce/entity/factory/implementation/CommonBrandFactoryImp.java
@@ -0,0 +1,13 @@
+package com.ndlcommerce.entity.factory.implementation;
+
+import com.ndlcommerce.entity.factory.interfaces.BrandFactory;
+import com.ndlcommerce.entity.model.implementation.CommonBrand;
+import com.ndlcommerce.entity.model.interfaces.Brand;
+
+public class CommonBrandFactoryImp implements BrandFactory {
+
+    @Override
+    public Brand create(String name) {
+        return new CommonBrand(name);
+    }
+}
